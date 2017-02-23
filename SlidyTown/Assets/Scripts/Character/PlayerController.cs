7b68b44e46568ae10141caaffe4b42130117960a
Andrey Chelushkin <achelushkin@andiks.com>
2017-02-23 17:16:30 +0300

    Added pickups

--- SlidyTown/Assets/Scripts/Character/PlayerController.cs
+++ SlidyTown/Assets/Scripts/Character/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : SingletonBehaviour<PlayerController> {
     }
 
     public event Action OnPlayerDied;
+    public event Action<PickupItem> OnPickedUp;
 
     public MovementController MovementController { get; private set; }
 
@@ -24,12 +25,20 @@ public class PlayerController : SingletonBehaviour<PlayerController> {
         UpdateInput();
     }
 
-    void OnTriggerEnter() {
-        MovementController.StopMovement();
-        Instantiate(WorldObjectProvider.GetWorldObject("DestroyEffect"), this.transform.position, Quaternion.identity);
-        this.transform.GetChild(0).gameObject.SetActive(false);
-        if (OnPlayerDied != null)
-            OnPlayerDied();
+    void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Pickup")) {
+            var pickup = collider.GetComponent<PickupItem>();
+            pickup.OnPickedUp();
+            if (OnPickedUp != null)
+                OnPickedUp(pickup);
+        }
+        //else {
+        //    MovementController.StopMovement();
+        //    Instantiate(WorldObjectProvider.GetWorldObject("DestroyEffect"), this.transform.position, Quaternion.identity);
+        //    this.transform.GetChild(0).gameObject.SetActive(false);
+        //    if (OnPlayerDied != null)
+        //        OnPlayerDied();
+        //}
     }
 
     private void UpdateInput() {
---
+++ SlidyTown/Assets/Scripts/Game/PickupSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour {
+
+    [Range(0,1)]
+    public float Probability;
+
+    void OnEnable() {
+        Clear();
+        if (Random.value < Probability)
+            Spawn();
+    }
+
+    private void Clear() {
+        for (int i = 0; i < this.transform.childCount; i++) {
+            foreach(Transform pickup in this.transform.GetChild(i)) {
+                Destroy(pickup.gameObject);
+            }
+        }
+    }
+
+    private void Spawn() {
+        for (int i = 0; i < this.transform.childCount; i++) {
+            var obj = Instantiate(WorldObjectProvider.GetWorldObject<GameObject>("Pickup"), this.transform.GetChild(i), false);
+            obj.transform.localPosition = Vector3.zero;
+            obj.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
