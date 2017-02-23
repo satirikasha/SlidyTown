48acdd7e7aadfc8cbb2d3ef56bfe6d48c8088d8e
Andrey Chelushkin <achelushkin@andiks.com>
2017-02-23 14:07:07 +0300

    WorldObject fixed

--- SlidyTown/Assets/Scripts/World/WorldObject.cs
+++ SlidyTown/Assets/Scripts/World/WorldObject.cs
@@ -12,6 +12,8 @@ public class WorldObject : MonoBehaviour {
         }
     }
 
+    private GameObject _CurrentGameObject;
+
     void Awake() {
         WorldObjectProvider.OnWorldChanged += Refresh;
         Refresh();
@@ -22,31 +24,38 @@ public class WorldObject : MonoBehaviour {
     }
 
     public void Refresh() {
+        var source = WorldObjectProvider.GetWorldObject<GameObject>(this.CleanName);
+        if (source != null) {
+            var obj = Instantiate(source, this.transform, false);
+            obj.name = this.CleanName + "_" + WorldObjectProvider.CurrentWorld;
+            _CurrentGameObject = obj;
+        }
+        else {
+            _CurrentGameObject = null;
+        }
+
         if (this.transform.childCount > 0) {
 #if UNITY_EDITOR
             if (Application.isPlaying) {
                 for (int i = this.transform.childCount - 1; i >= 0; i--) {
-                    Destroy(this.transform.GetChild(i).gameObject);
+                    if (_CurrentGameObject != this.transform.GetChild(i).gameObject)
+                        Destroy(this.transform.GetChild(i).gameObject);
                 }
             }
             else {
                 UnityEditor.EditorApplication.delayCall += () => {
                     for (int i = this.transform.childCount - 1; i >= 0; i--) {
-                        DestroyImmediate(this.transform.GetChild(i).gameObject);
+                        if (_CurrentGameObject != this.transform.GetChild(i).gameObject)
+                            DestroyImmediate(this.transform.GetChild(i).gameObject);
                     }
                 };
             }
 #else
             for (int i = this.transform.childCount - 1; i >= 0; i--) {
-                Destroy(this.transform.GetChild(i).gameObject);
+                if (_CurrentGameObject != this.transform.GetChild(i).gameObject)
+                    Destroy(this.transform.GetChild(i).gameObject);
             }
 #endif
-        }
-
-        var source = WorldObjectProvider.GetWorldObject<GameObject>(this.CleanName);
-        if (source != null) {
-            var obj = Instantiate(source, this.transform, false);
-            obj.name = this.CleanName + "_" + WorldObjectProvider.CurrentWorld;
         }
     }
 }
