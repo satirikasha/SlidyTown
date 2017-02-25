d84de10476024f8b58abe48a3978e7c4f5c48615
satirikasha <satirikasha@gmail.com>
2017-02-25 20:14:53 +0300

    New duck trail

---
+++ SlidyTown/Assets/Scripts/Enviroment/Common/AlphaBySpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaBySpeed : MonoBehaviour {
+
+    public Material ControlledMaterial { get; private set; }
+
+	void Awake() {
+        var renderer = this.GetComponent<Renderer>();
+        ControlledMaterial = new Material(renderer.sharedMaterial);
+        renderer.sharedMaterial = ControlledMaterial;
+    }
+
+    void Update() {
+        ControlledMaterial.SetColor("_Color", new Color(1, 1, 1,
+            (PlayerController.LocalPlayer.MovementController.Speed - PlayerController.LocalPlayer.MovementController.MinSpeed) /
+            (PlayerController.LocalPlayer.MovementController.MaxSpeed - PlayerController.LocalPlayer.MovementController.MinSpeed))
+            );
+    }
+}
