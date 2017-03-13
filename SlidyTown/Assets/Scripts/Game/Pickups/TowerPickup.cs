0a8f1e679ac4b83450029cc8c06c140e9c8546d1
Andrey Chelushkin <achelushkin@andiks.com>
2017-03-13 18:49:56 +0300

    Last worlds polishing

---
+++ SlidyTown/Assets/Scripts/Game/Pickups/TowerPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPickup : PickupItem {
+
+    public override void OnPickedUp() {
+        base.OnPickedUp();
+        var splashEffect = VisualEffectsManager.Instance.GetEffect("PickupDestroyed");
+        splashEffect.transform.position = this.transform.position;
+        splashEffect.Play();
+    }
+}
--- SlidyTown/Assets/Scripts/UI/WidgetVisibilityController.cs
+++ SlidyTown/Assets/Scripts/UI/WidgetVisibilityController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(100)]
 public class WidgetVisibilityController : MonoBehaviour {
 
+    public bool LogoVisible;
     public bool CoinWidgetVisible;
 
 	void OnEnable() {
+        LogoWidget.Instance.SetVisibility(LogoVisible);
         CoinWidget.Instance.SetVisibility(CoinWidgetVisible);
     }
 }
