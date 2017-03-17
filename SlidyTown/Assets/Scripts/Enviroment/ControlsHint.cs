1aa3c4a230044695194cff9f16c0a9dbcd4d7041
Andrey Chelushkin <achelushkin@andiks.com>
2017-03-17 20:03:01 +0300

    Added controls hint

---
+++ SlidyTown/Assets/Scripts/Enviroment/ControlsHint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsHint : MonoBehaviour {
+
+    void Awake() {
+        if (SaveManager.Data.MaxPoints > 5)
+            this.gameObject.SetActive(false);
+    }
+}
