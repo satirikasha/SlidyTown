498a30769ff94e5e8c9b08295a58d6a45a2fa056
Andrey Chelushkin <achelushkin@andiks.com>
2017-04-11 21:46:31 +0300

    Added money pickup

--- SlidyTown/Assets/Scripts/Visual/DriftController.cs
+++ SlidyTown/Assets/Scripts/Visual/DriftController.cs
@@ -25,7 +25,7 @@ public class DriftController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        _TurnDir = Vector3.Dot(_Parent.forward, _PreviousRight) * 7;
+        _TurnDir = Vector3.Dot(_Parent.forward, _PreviousRight) * 5;
         _TargetAngVelocity = 650 * PlayerController.LocalPlayer.MovementController.NormalizedSpeed;
         _TargetAngVelocity *= _TurnDir;
         _TargetAngVelocity += -Vector3.Dot(_Transform.forward, _Parent.right) * 650;
